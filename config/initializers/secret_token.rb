93225b799e144db72c606f6133ae6bac6dbcddf0
Amanda Li <manda986@gmail.com>
2013-10-10 00:08:55 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nycuniteddragons::Application.config.secret_key_base = '8bb3db1af75ed9678cfde02e2189a2459925c020263032a90292268e7110026b4c7a833073bef161d0a15b9c81de185cbc128141ebe2fe614abae27490d14bf4'
